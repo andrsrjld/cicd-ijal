a858afa6d8283bcd9d5ef00449974efdda9e4e30
andrsrjld <andreasallbuqori@gmail.com>
2021-10-04 22:50:03 +0700

    febra kontol

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          ijal ganteng
+          febra kontol
         </p>
         <a
           className="App-link"
